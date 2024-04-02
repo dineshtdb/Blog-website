570b33ff37a0bb7da4b57d9464373de9e7ff2862
dineshtdb <dineshtdb@gmail.com>
2024-04-03 01:39:44 +0530

    auth mudunchu

--- api/Index.js
+++ api/Index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const User=require('./models/User');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+
 const app = express();
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:5173'}));
 app.use(express.json());
+const salt=bcrypt.genSaltSync(10);
+const secret='hvhscvvsacvvsa';
 
 
 mongoose.connect('mongodb+srv://dineshutd:Dinesh29062004@cluster0.ja5gmbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
@@ -12,9 +17,36 @@ mongoose.connect('mongodb+srv://dineshutd:Dinesh29062004@cluster0.ja5gmbm.mongod
 
 app.post('/register', async(req, res) => {
     const { username, password } = req.body;
-   const user= await User.create({username , password});
-    res.json(user);
+    try{
+   const user= await User.create({username , 
+    password:bcrypt.hashSync(password,salt),
 });
+    res.json(user);
+    }catch(e){
+        res.status(400).json(e);
+    }
+});
+app.post('/login', async(req, res) => {
+    const { username, password } = req.body;
+    
+        const user = await User.findOne({ username });
+        if (!user) {
+            // User not found with the provided username
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const passok = bcrypt.compareSync(password, user.password);
+        if(passok){
+            jwt.sign({username,id:user.id},secret,{},(err,token)=>{
+                    if(err) throw err;
+                    res.cookie('token',token).json('ok');
+            })
+        }else{
+           res.status(400).json("Wrong");
+        }
+    
+});
+
+
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 export default function Login(){
+    const [username,setUsername]=useState('');
+    const [password,setPassword]=useState('');
+    const [redirect,setredirect]=useState(false);
+    async function login(ev) {
+        ev.preventDefault();
+        const response=await fetch('http://localhost:4000/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials:'include',
+        });
+        if(response.ok){
+            setredirect(true);
+        }else{
+            alert('worng')
+        }
+    }
+    if(redirect){
+        return <Navigate to={'/'}/>
+    }
+
     return(
         <div>
-            <form className="login">
+            <form className="login" onSubmit={login}>
                 <h1>Login</h1>
-                <input type="text" placeholder="username"/>
-                <input type="text" placeholder="paswword"/>
+                <input type="text"
+                 placeholder="username"
+                 value={username}
+                 onChange={ev => setUsername(ev.target.value)}
+                 />
+                <input type="text"
+                 placeholder="paswword"
+                 value={password}
+                  onChange={ev => setPassword(ev.target.value)}/>
                 <button>Login</button>
             </form>
         </div>
