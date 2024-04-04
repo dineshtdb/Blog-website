9b541e409d55c3695bdc883bc681128613133c69
dineshtdb <dineshtdb@gmail.com>
2024-04-04 09:25:49 +0530

    ule po

--- api/Index.js
+++ api/Index.js
@@ -4,13 +4,13 @@ const { default: mongoose } = require('mongoose');
 const User=require('./models/User');
 const bcrypt=require('bcryptjs');
 const jwt=require('jsonwebtoken');
-
+const cookieparser =require('cookie-parser');
 const app = express();
 app.use(cors({credentials:true,origin:'http://localhost:5173'}));
 app.use(express.json());
 const salt=bcrypt.genSaltSync(10);
 const secret='hvhscvvsacvvsa';
-
+app.use(cookieparser());
 
 mongoose.connect('mongodb+srv://dineshutd:Dinesh29062004@cluster0.ja5gmbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
@@ -26,28 +26,35 @@ app.post('/register', async(req, res) => {
         res.status(400).json(e);
     }
 });
-app.post('/login', async(req, res) => {
-    const { username, password } = req.body;
-    
-        const user = await User.findOne({ username });
-        if (!user) {
-            // User not found with the provided username
-            return res.status(404).json({ message: 'User not found' });
-        }
-        const passok = bcrypt.compareSync(password, user.password);
-        if(passok){
-            jwt.sign({username,id:user.id},secret,{},(err,token)=>{
-                    if(err) throw err;
-                    res.cookie('token',token).json('ok');
-            })
-        }else{
-           res.status(400).json("Wrong");
-        }
-    
-});
-
-
-
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      // logged in
+      jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('wrong credentials');
+    }
+  });
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) => {
+      if (err) throw err;
+      res.json(info);
+    });
+  });
+  
+  app.post('/logout', (req,res) => {
+    res.cookie('token', '').json('ok');
+  });
+ 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
 });
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { createContext,useState } from 'react'
 import './App.css'
 import Post from './Post';
 import Header from './Header';
@@ -7,15 +7,20 @@ import Layout from './Layout';
 import Indexpage from './pages/Indexpage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import {UserContextProvider} from './UserContext';
 function App() {
   return (
-  <Routes>
+    <UserContextProvider>
+ <Routes>
     <Route path='/' element={<Layout/>}>
     <Route index element={<Indexpage/>}/>
     <Route path={'/login'} element={<Login/>}/>
 <Route path={'/register'} element={<Register/>}/>
+
     </Route>
   </Routes>  
+    </UserContextProvider>
+ 
   
   );
 }
